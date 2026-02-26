3874ce6f716ef4fa23eb0a29e509de873cbbf950
Alesya98 <121381968+Alesya98@users.noreply.github.com>
2026-02-26 14:31:52 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import store from './redux/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename='/To-Do-List-Redux_RTK_API'>
+    <BrowserRouter basename="/To-Do-List-Redux_RTK_API/">
  <Provider store={store}>
   <App />
  </Provider>
