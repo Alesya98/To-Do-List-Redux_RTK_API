d7979971e67e230a40eb061a723dc805d2682998
Alesya98 <petrovskaya_alesya98@mail.ru>
2026-02-26 14:14:15 +0300

    corrections

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
  <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/To-Do-List-Redux_RTK_API/'>
   <App />
   </BrowserRouter>
  </Provider>
