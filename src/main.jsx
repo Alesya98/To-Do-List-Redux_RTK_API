951b81ddc1d4653bb4dda1c5c83b7f8242096ecb
Alesya98 <petrovskaya_alesya98@mail.ru>
2026-03-02 10:09:38 +0300

    green

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouterRouter basename="/To-Do-List-Redux_RTK_API/">
+  <HashRouter basename="/To-Do-List-Redux_RTK_API/">
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouterRouter>,
+  </HashRouter>,
 );
