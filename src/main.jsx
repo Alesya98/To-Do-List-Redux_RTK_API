fc9f4382602a3b452e50ab9b085b18e552d0e520
Alesya98 <petrovskaya_alesya98@mail.ru>
2026-03-02 10:14:37 +0300

    greens

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/To-Do-List-Redux_RTK_API/">
+  <BrowserRouter basename="/To-Do-List-Redux_RTK_API/">
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
